d95405be9f0fba28489fcc3e04273ec77317e0fd
demdani <demjen.daniel@otpmobil.com>
2025-01-15 19:40:21 +0100

    Create project

---
+++ StaggeredList/StaggeredListApp.swift
@@ -0,0 +1,17 @@
+//
+//  StaggeredListApp.swift
+//  StaggeredList
+//
+//  Created by Demjén Dániel on 15/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct StaggeredListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
